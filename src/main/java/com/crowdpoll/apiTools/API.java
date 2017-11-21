5a2ec5e09cb0d3afef9fc957dd8e9ea89174f78f
Chris Broome <chris.a.broome@gmail.com>
2017-11-20 21:13:05 -0500

    Boilerplate Application file and basic api structure

---
+++ src/main/java/com/crowdpoll/apiTools/API.java
@@ -0,0 +1,7 @@
+package com.crowdpoll.apiTools;
+
+public interface API {
+
+    void search();
+
+}
---
+++ src/main/java/com/crowdpoll/kiva/KivaAPI.java
@@ -0,0 +1,12 @@
+package com.crowdpoll.kiva;
+
+import com.crowdpoll.apiTools.API;
+
+public class KivaAPI implements API {
+
+    public void search() {
+
+    }
+
+
+}
