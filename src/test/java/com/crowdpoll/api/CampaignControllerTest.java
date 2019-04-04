e8d9d2d44bd5c581e19d6d2e0d2627a2c4a9579f
Chris <chris.a.broome@gmail.com>
2019-04-04 06:56:46 -0400

    Getting junit hooked up with a simple test

---
+++ src/test/java/com/crowdpoll/api/CampaignControllerTest.java
@@ -0,0 +1,25 @@
+package com.crowdpoll.api;
+
+import com.crowdpoll.entities.Campaign;
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+
+public class CampaignControllerTest {
+
+
+    @Test
+    public void shouldCallGetActive() {
+        List<Campaign> campaigns = new ArrayList<Campaign>();
+        CampainController campainController = mock(CampainController.class);
+        when(campainController.activeCampaigns()).thenReturn(campaigns);
+        assert( campainController.getActive() ).equals( campaigns );
+    }
+
+
+}
